6eb14236c67bd3cd8b6a33a457134d8f1f8c5383
Ste Royle <steroyle@gmail.com>
2016-02-26 20:17:46 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Bootstrap + Sass + Gulp + Bower
+A basic starting point for using Bootstrap with Sass, Gulp and Bower.
+
+## Install
+If you haven't already, install Gulp and Bower globally.
+```
+npm install -g gulp bower
+```
+Install Bower and npm packages locally into your project.
+
+```
+bower install
+npm install
+```
+
+## Run
+In the project root run the following:
+```
+gulp
+```
---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var gulp    = require('gulp');
+var sass    = require('gulp-sass');
+var concat  = require('gulp-concat');
+var uglify  = require("gulp-uglify");
+var minifycss = require("gulp-minify-css");
+
+// folders
+var source = 'src/';
+var dest = 'dist/';
+
+// Bootstrap source
+var bootstrapPath = './bower_components/bootstrap-sass/';
+
+//sass options
+
+var sassOptions = {
+  outputStyle: 'nested',
+  precison: 3,
+  errLogToConsole: true,
+  includePaths: [bootstrapPath + 'assets/stylesheets']
+};
+
+gulp.task('sass', function () {
+  return gulp.src(source + 'css/**/*.scss')
+    .pipe(sass(sassOptions).on('error', sass.logError))
+    .pipe(minifycss())
+    .pipe(gulp.dest(dest + 'css/'));
+});
+
+//scripts
+gulp.task('scripts', function() {
+  return gulp.src([bootstrapPath + 'assets/javascripts/bootstrap.min.js', source + 'js/**/*.js'])
+    .pipe(concat('scripts.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(dest + 'js/'));
+});
+
+// copy bootstrap fonts to dest
+gulp.task('fonts', function () {
+  return gulp
+    .src([source + 'fonts/*.*', bootstrapPath + 'assets/fonts/**/*'])
+    .pipe(gulp.dest(dest + 'fonts/'));
+});
+
+// default task
+gulp.task('default', ['sass', 'scripts', 'fonts'], function () {
+   gulp.watch([source + 'css/**/*'], ['sass']);
+   gulp.watch([source + 'js/**/*'], ['scripts']);
+   gulp.watch([source + 'fonts/**/*'], ['fonts']);
+});
